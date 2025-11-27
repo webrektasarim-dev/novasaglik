0d54a48e2cbc46998628c5886e490d2989d096cb
webrektasarim <webrektasarim@gmail.com>
2025-11-27 13:32:16 +0300

    Chore: Add favicon logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
-        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🏥</text></svg>" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <meta name="theme-color" content="#1e3a5f" />
         <script
           type="application/ld+json"
